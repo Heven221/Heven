fbb8a1490d7b1627579fb4b4195a365ea70b2f9b
HEVEN-STORES <heven.storess@gmail.com>
2025-03-31 16:07:43 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,6 @@ ReactDom.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
         <ShopContextProvider>
                <App />
-          <iframe src="https://bot.orimon.ai/?tenantId=d00af6b1-a18a-4d6b-a312-78a2bfa46356&fullScreenBot=true" height="100%" width="100%" frameBorder="0" border="none"></iframe>
 
 
        </ShopContextProvider>

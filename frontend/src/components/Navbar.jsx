8f3b2550633d18e759d04d82233e37c7a05dc0a7
HEVEN-STORES <heven.storess@gmail.com>
2025-05-21 15:38:54 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -81,6 +81,8 @@ const Navbar = () => {
                 <NavLink onClick={()=>setVisible(false)}className='py-4 pl-6 border'to='/orders'>Orders</NavLink>
                 <NavLink onClick={()=>setVisible(false)}className='py-4 pl-6 border'to='/about'>ABOUT</NavLink>
                 <NavLink onClick={()=>setVisible(false)}className='py-4 pl-6 border'to='/contact'>CONTACT</NavLink>
+                <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
+
 
 
             </div>

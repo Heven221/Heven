27d678631e0faabceef66b7cf25f4a7783e9c909
HEVEN-STORES <heven.storess@gmail.com>
2025-05-31 22:26:46 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 export const ShopContext = createContext();
-
+       
 const ShopContextProvider = (props)=>{
     const currency = '₹ ';
     const delivery_fee = 10;

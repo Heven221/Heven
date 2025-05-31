47565939ad7af74d46df5a6a553c43f4e92f1f4e
HEVEN-STORES <heven.storess@gmail.com>
2025-05-31 23:48:08 +0530

    Update Orders.jsx

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -68,6 +68,8 @@ const Orders = () => {
                         </div>
                         <p className='mt-1'>Date: <span className='text-gray-400 '>{new Date(item.date).toDateString()}</span></p>
                         <p className='mt-1'>Payment: <span className='text-gray-400 '>{item.paymentMethod}</span></p>
+                        <p className='mt-1'>Payment: <span className='text-gray-400 '>{item.payment ? 'Done':'Pending'}</span></p>
+
                       </div>
 
                 </div>

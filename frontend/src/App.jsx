8b786385f8c583072b34965c166ef190d034886d
Heven221 <hevenai221@gmail.com>
2025-06-17 23:03:27 +0530

    Updated vite.config.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,10 @@
 import React from 'react'
-import { Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Collections from './pages/Collections'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Cart from './pages/Cart'
-import Login from './pages/Login'
-import Orders from './pages/Orders'
-import PlaceOrder from './pages/PlaceOrder'
-import Product from './pages/Product'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import SearchBar from './components/SearchBar'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-
-const App = () => {
+function App() {
   return (
-    <div className='px-4 sm:px-[5vw] md:px[7vw] lg:px-[9vw]'>
-      <ToastContainer/>
-      <Navbar/>
-      <SearchBar/>
-
-
-
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/collections' element={<Collections/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/orders' element={<Orders/>}/>
-        <Route path='/placeorder' element={<PlaceOrder/>}/>
-        <Route path='/product/:productId' element={<Product/>}/>
-
-      </Routes>
-      <Footer/>
+    <div className="App">
+      <h1>Frontend Application</h1>
+      <p>The frontend server is now running successfully!</p>
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,10 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
 import React from 'react'
-import ReactDom from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import App  from './App'
-import ShopContextProvider from './context/ShopContext.jsx'
-ReactDom.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-        <ShopContextProvider>
-               <App />
 
-
-       </ShopContextProvider>
-   
-  </BrowserRouter>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
